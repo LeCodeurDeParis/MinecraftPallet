b14cf8fce2997d3db43f6c2c37276fa59398f7fb
Paul Boisaubert <146094035+LeCodeurDeParis@users.noreply.github.com>
2024-09-22 14:52:39 +0200

    comit

--- Site Palette Minecraft/public/JS/accueil.js
+++ Site Palette Minecraft/public/JS/accueil.js
@@ -1,49 +1,53 @@
-const caseBlock = document.querySelectorAll('.Block')
-const lockButton = document.querySelectorAll('.lock')
-const buttonRandom = document.getElementById('generate')
+document.addEventListener('DOMContentLoaded', async () => {
+    const caseBlock = document.querySelectorAll('.block')
+    const lockButton = document.querySelectorAll('.lock')
+    const buttonRandom = document.getElementById('generate')
 
-function randomInt(min, max) {
-    return min + Math.floor((max - min) * Math.random());
-}
-
-async function getBlock(){
-    let response = await fetch('/api/data');
-    let data = await response.json();
-    return data;
-}
-
-
-async function displayBlockRandom(){
-    let data = await getBlock()
-    console.log(data)
-    caseBlock.forEach(element => {
-        if (!element.classList.contains('locked')) {
-            let getRandomBlock = randomInt(0, data.length)
-            console.log(getRandomBlock)
-            element.innerHTML = `
-            <img src="${data[getRandomBlock].image}" alt="${data[getRandomBlock].nom}">
-            `
-        }
-    });
-}
-
-function toggleLock(event) {
-    const button = event.target;
-    const block = button.closest('.color'); // Trouver le bloc parent
-
-    if (button.classList.contains('locked')) {
-        // Déverrouiller
-        button.classList.remove('locked');
-        button.textContent = 'Lock';
-    } else {
-        // Verrouiller
-        button.classList.add('locked');
-        button.textContent = 'Unlock';
+    function randomInt(min, max) {
+        return min + Math.floor((max - min) * Math.random());
     }
-}
 
-buttonRandom.addEventListener('click', displayBlockRandom)
+    async function getBlock() {
+        let response = await fetch('/api/data');
+        let data = await response.json();
+        return data;
+    }
 
-lockButton.forEach(button => {
-    button.addEventListener('click', toggleLock);
-});
+    let allBlock = await getBlock()
+
+    async function displayBlockRandom() {
+        caseBlock.forEach(element => {
+            if (!element.classList.contains('locked')) {
+                console.log(element)
+                let getRandomBlock = randomInt(0, allBlock.length)
+                let img = element.querySelector('img')
+                img.src = allBlock[getRandomBlock].image
+                img.alt = allBlock[getRandomBlock].nom
+            }
+            else {
+                console.log('bloqué')
+            }
+        });
+    }
+
+    function toggleLock(event) {
+        const button = event.target;
+        const block = button.parentElement.parentElement; // Trouver le bloc parent
+
+        if (block.classList.contains('locked')) {
+            // Déverrouiller
+            block.classList.remove('locked');
+            button.textContent = 'Lock';
+        } else {
+            // Verrouiller
+            block.classList.add('locked');
+            button.textContent = 'Unlock';
+        }
+    }
+
+    buttonRandom.addEventListener('click', displayBlockRandom)
+
+    lockButton.forEach(button => {
+        button.addEventListener('click', toggleLock);
+    });
+})
